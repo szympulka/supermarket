35c451b9b6e76b4f8fd4e5db065c8adbe585c392
szympulka@gmail.com <durabrite95>
2015-08-05 23:04:42 +0200

    v2.0

--- Sklep/Admin.cpp
+++ Sklep/Admin.cpp
@@ -5,27 +5,96 @@
 #include <cstdlib>
 #include <conio.h>
 #include <string>
-void deletee()
+#include <windows.h>
+
+
+void delete1()
 {
+
+	int choose = 0;
+	char agreement;
+	cout << "Ktory plik chcesz usunac ?"
+		<< "\n[1]Warzywa"
+		<< "\n[2]Owoce"
+		<< "\n[3}Alkohol"
+		<< "\n[4]Napoje"
+		<< "\n[5]Suche";
+	cin.clear();
+	cin.sync();
+	choose = _getch();
+	cout << "\nCzy jestes pewien ze chcesz usunac ten plik?: Y|N";
+	cin >> agreement;
+	if ( agreement == 'y' )
+	{
+		
+		if (choose = 1)  DeleteFileA("C:\\Users\\pulso\\Documents\\Visual Studio 2015\\Projects\\supermarket\\Sklep\\Warzywa.txt");  
+		if (choose = 2)  DeleteFileA("C:\\Users\\pulso\\Documents\\Visual Studio 2015\\Projects\\supermarket\\Sklep\\Owoce.txt");    
+		if (choose = 3)  DeleteFileA("C:\\Users\\pulso\\Documents\\Visual Studio 2015\\Projects\\supermarket\\Sklep\\Alkohol.txt");  
+		if (choose = 4)  DeleteFileA("C:\\Users\\pulso\\Documents\\Visual Studio 2015\\Projects\\supermarket\\Sklep\\Napoje.txt");  
+		if (choose = 5)  DeleteFileA("C:\\Users\\pulso\\Documents\\Visual Studio 2015\\Projects\\supermarket\\Sklep\\Suche.txt");   
+		cout << "\nPlik usunieto pomyslnie!\n";
+		
+	}
+	else
+		cout << "\nNie usunieto zadnego pliku!\n\n";
 
 }
 void change()
-{
+{ 
+	char agree;
+	cout << "czy na pewno chcesz zmienic login i haslo ?: Y|N";
+	cin >> agree;
+	if (agree == 'y')
+	{
+		string login;
+		DeleteFileA("C:\\Users\\pulso\\Documents\\Visual Studio 2015\\Projects\\supermarket\\Sklep\\admin.txt");
+		fstream plik;
+		plik.open("Admin.txt", ios::in | ios::out);
+		cout << "\nPodaj Login:"; cin >> login;
+		cout << "\nPodaj haslo:";
+		short unsigned int status;
+		char actual;
+		string input;
+		do {
+			actual = NULL;
+			actual = _getch();
+			status = static_cast <int>(actual);
+			if (status == 13)
+				break;
 
+			input += actual;
+			cout << "*";
+		} while (1);
+
+		plik << login;
+		plik << "\n";
+		plik << input;
+		system("cls");
+		cout << "\nLogin i haslo zostaly zmienione!\n\n";
+	}
+	else
+	{
+		system("cls");
+		cout << "Login i haslo nie zostalo zmienione\n\n";
+	}
 }
 
 void menu()
 {
+	cin.clear();
+	cin.sync();
 	system("cls");
 	int menu;
 	cout << "\nWitaj w panelu administracyjnym:"
 		<< "\n[1]Usun plik"
 		<< "\n[2]Zmien login i haslo:";
 	menu = _getch();
+
+
 	switch (menu)
 	{
-	case '1': system("cls"); deletee();
-	case '2': system("cls"); change();
+	case '1': system("cls"); delete1();				break;
+	case '2': system("cls"); change();				break;
 	}
 }
 void check(string input, string login)
@@ -53,7 +122,7 @@ void check(string input, string login)
 		system("cls");
 		cout << "Podales zly login lub haslo\n\n";
 	}
-	
+	plik.close();
 }
 
 void password()
